be2a7eb0202f45cd71633ccd24101e2f4ae2c113
NgoDuc <ngoduc202@gmail.com>
2023-06-12 11:56:50 +0700

    Upload: Header home, basic setup

--- src/Components/Header-home/HeaderHome.tsx
+++ src/Components/Header-home/HeaderHome.tsx
@@ -1,15 +1,37 @@
 // import React from 'react'
-
+import './headerHome.scss'
 function HeaderHome() {
-  return (
-    <div>
-        <div className="left-header">
-        <img src="/src/assets/Image/Airbnb_logo.png" alt="..." />
+    return (
+        <div className='header-home'>
+            <div className="container-header">
+                <div className="left-header">
+                    <img src="/src/assets/Image/Airbnb_logo.png" alt="..." />
+                </div>
+                <div className="mid-header">
+                    <div className="location-random">
+                        <p>Địa điểm bất kỳ</p>
+                    </div>
+                    <div className="week">
+                        <p>tuần bất kỳ</p>
+                    </div>
+                    <div className="add-customer">
+                        <p>Thêm khách</p>
+                        <button className='search-btn'>
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </div>
+                </div>
+                <div className="right-header">
+                    <p>Cho thuê chỗ ở qua Airbnb</p>
+                    <i className="fa-solid fa-globe"></i>
+                    <div className="login-area">
+                        <i className="fa-solid fa-bars"></i>
+                        <i className="fa-solid fa-user user-login"></i>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className="mid-header"></div>
-        <div className="right-header"></div>
-    </div>
-  )
+    )
 }
 
 export default HeaderHome
