48f07e17e384caa6ca39b1f0c85df0344051c651
NgoDuc <ngoduc202@gmail.com>
2023-06-21 08:45:34 +0700

    Update: some config

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -1,4 +1,5 @@
 export const CYBER_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCDEkMOgIE7hurVuZyAwNyIsIkhldEhhblN0cmluZyI6IjA0LzExLzIwMjMiLCJIZXRIYW5UaW1lIjoiMTY5OTA1NjAwMDAwMCIsIm5iZiI6MTY2OTQ4MjAwMCwiZXhwIjoxNjk5MjAzNjAwfQ.z53DwWShTQ-NYmv_cyVwxzyaarjOV3xiMrElt3gwl8M'
+export const ACCESS_TOKEN = 'accessToken'
 
 export interface IRoomDetail{
     id: number,
