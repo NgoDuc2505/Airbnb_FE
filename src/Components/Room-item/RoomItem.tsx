e7ec0ae4ef2f730c426787890f99683e34e02e62
dkav0206 <dkhoa2019vn@gmail.com>
2023-06-27 19:05:10 +1000

    fix room  template

--- src/Components/Room-item/RoomItem.tsx
+++ src/Components/Room-item/RoomItem.tsx
@@ -19,7 +19,7 @@ function RoomItem({ listRoomItem }:IProps) {
         <div className="room-item-content">
             <div className="room-item-title">
                 <p>Toàn bộ căn hộ, khách sạn tại địa điểm</p>
-                <NavLink to={`/Detail/` + listRoomItem.id}><h2>{listRoomItem.tenPhong}</h2></NavLink>
+                <NavLink to={`/room/` + listRoomItem.id}><h2>{listRoomItem.tenPhong}</h2></NavLink>
                 
             </div>
             <hr className='hr-item' />
