99309aa31fe27dcca02e05c06ccf84b44425ab5b
dkav0206 <dkhoa2019vn@gmail.com>
2023-07-04 00:08:42 +1000

    responsive for comment

---
+++ src/Components/CardSlider/cardslider.tsx
@@ -0,0 +1,81 @@
+import { IComment } from '../../constant/constant';
+import { Comment, checkIfImageExists } from '../Detail/commentComponent';
+import './cardslider.scss'
+
+interface IProps { 
+    comment: IComment[] | any,
+    sliceMax: number,
+    limit: boolean
+}
+
+
+export function CardSlider({comment, sliceMax, limit}: IProps){ 
+    let items : any= document.querySelectorAll('.calslider .calitem') ;
+    let next :any = document.getElementById('next');
+    let prev : any= document.getElementById('prev');
+    
+    let active = 0;
+    function loadShow(){
+        let stt = 0;
+        items[active].style.transform = `none`;
+        items[active].style.zIndex = 1;
+        items[active].style.filter = 'none';
+        items[active].style.opacity = 1;
+        for(var i = active + 1; i < items.length; i++){
+            stt++;
+            items[i].style.transform = `translateX(${120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(-1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+        stt = 0;
+        for(var i = active - 1; i >= 0; i--){
+            stt++;
+            items[i].style.transform = `translateX(${-120*stt}px) scale(${1 - 0.2*stt}) perspective(16px) rotateY(1deg)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.filter = 'blur(5px)';
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+    }
+    try{  
+        loadShow();
+        next.onclick = function(){
+            active = active + 1 < items.length ? active + 1 : active;
+            loadShow();
+        }
+        prev.onclick = function(){
+            active = active - 1 >= 0 ? active - 1 : active;
+            loadShow();
+        }
+    
+    
+    } catch{}
+        
+  
+    return(
+        <div className="calslider">
+           {
+                comment && comment.slice(0, sliceMax).map((currentComment: IComment, index: number) => { 
+                    return ( 
+                        <div className= 'calitem' key={index}>
+                            <div className='calSection'>
+                                <p>{limit ? currentComment.noiDung.slice(0, 300) + (currentComment.noiDung.length > 50 ? "..." : "") : currentComment.noiDung}</p>
+                                <div className='cal-item-ava'>
+                                    <img src={checkIfImageExists(currentComment.avatar)} alt="" />
+                                    <div>
+                                        <h3>{currentComment.tenNguoiBinhLuan}</h3>
+                                        <p>{currentComment.ngayBinhLuan}</p>
+                                    </div>
+                                </div>
+                            </div>
+                           
+                        </div>
+                        
+                    )
+                })
+            }
+            <button id="next">{`>`}</button>
+            <button id="prev">{`<`}</button>
+        </div>
+    )
+}
