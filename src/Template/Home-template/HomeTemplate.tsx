70700671e0d1d3206f3783adb81baed1669d44cc
NgoDuc <ngoduc202@gmail.com>
2023-07-13 11:47:40 +0700

    update: list room skeleton

--- src/Template/Home-template/HomeTemplate.tsx
+++ src/Template/Home-template/HomeTemplate.tsx
@@ -1,15 +1,15 @@
 import HeaderHome from "../../Components/Header-home/HeaderHome"
-import {Suspense} from 'react'
-import {Outlet} from 'react-router-dom'
+import {Suspense, useEffect} from 'react'
+import {Outlet, useLocation} from 'react-router-dom'
 import FooterHome from "../../Components/Footer-home/FooterHome"
-import {SkeletonC} from "../../Components/Skeleton/Skeleton"
+import {SkeletonC, SkeletonListRoom} from "../../Components/Skeleton/Skeleton"
 
 function HomeTemplate() {
-  
+  const location = useLocation()
   return (
     <div>
         <HeaderHome/>
-        <Suspense fallback={<SkeletonC/>}>
+        <Suspense fallback={location.pathname === '/' ? <SkeletonC/> : <SkeletonListRoom/>}>
             <Outlet />
         </Suspense>
         <FooterHome/>
