37137aa6af1293490094e2041342e1de94930b7f
dkav0206 <dkhoa2019vn@gmail.com>
2023-07-16 00:16:58 +1000

    comment update

--- src/Pages/Detail-room/DetailRoomLogic.ts
+++ src/Pages/Detail-room/DetailRoomLogic.ts
@@ -7,7 +7,7 @@ import { AppDispatch, RootState } from '../../redux/store'
 //const
 import { IComment, ILocationItem, IRoomDetail, ICommentId } from '../../constant/constant'
 import { getRoomById } from '../../redux/Detail-slice/DetailSlice';
-import { getCommentByRoomId, getCommentList } from '../../redux/Comment-slice/CommentSlice';
+import { appendCommentID, getCommentByRoomId, getCommentList } from '../../redux/Comment-slice/CommentSlice';
 
 export const useRoomDetail = ()=>{
     const idRoom = useParams()
@@ -29,6 +29,7 @@ export const useCommentRoom = () => {
     const stateComment : IComment[] = useSelector((state: RootState)=>state.sliceComment.currentRoomComment)
     const listComment: ICommentId[] = useSelector((state: RootState)=>state.sliceComment.listComment)
     const listCommentSortById :ICommentId[] = listComment.filter((item: ICommentId)=>{return item.maPhong === Number(idRoom.idDetail)})
+    dispatch(appendCommentID(listCommentSortById))
     return {stateComment, listCommentSortById}
 }
 
